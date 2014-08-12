9e65797edc5ac90841be0239da7a5a2733c742bf
MukundRaghavSharma <mukundraghavsharma@199-119-245-176.bobbroadband.com>
2014-08-11 21:57:41 -0500

    pain in da ass

--- yahoo_finance_attributes.py
+++ yahoo_finance_attributes.py
@@ -2,109 +2,108 @@ attributes = {
 
 # Pricing Dividends #
 # ----------------- #
-'a'  : 'Ask',
-'y'  : 'Dividend Yield'
-'b'  : 'Bid',
-'d'  : 'Dividend per Share',
-'b2' : 'Ask-Realtime'	
-'r1' : 'Dividend Pay Date',
-'b3' : 'Bid-Realtime',
-'q'  : 'Ex-Dividend Date',
-'p'  : 'Previous Close',
-'o'  : 'Open',	
+'Ask' 											:  'a',
+'Dividend Yield' 								:  'y', 
+'Bid' 											:  'b',  
+'Dividend per Share'							:  'd',
+'Ask-Realtime'									: 'b2', 
+'Dividend Pay Date'								: 'r1',
+'Bid-Realtime'									: 'b3',
+'Ex-Dividend Date'								:  'q',
+'Previous Close'								:  'p',
+'Open'											:  'o',	
 
 # Dates #
 # ----- #
-'c1' : 'Change',	
-'d1' : 'Last Trade Date',
-'c'  : 'Change & Percent Change',
-'d2' : 'Trade Date',
-'c6' : 'Change-Realtime',
-'t1' : 'Last Trade Time',
-'k2' : 'Change Percent-Realtime',
-'p2' : 'Change in Percent',
+'Change' 										: 'c1',	
+'Last Trade Date'								: 'd1',
+'Change & Percent Change'						:  'c',
+'Trade Date'									: 'd2',
+'Change-Realtime'								: 'c6',
+'Last Trade Time'								: 't1',
+'Change Percent-Realtime'						: 'k2',
+'Change in Percent'								: 'p2',
 
-# averages #
+# Averages #
 # -------- #
-'c8' : 'After Hours Change-Realtime'
-'m5' : 'Change From 200 Day Moving Average'
-'c3' : 'Commission'	
-'m6' : 'Percent Change From 200 Day Moving Average'
-'g'  : 'Day’s Low'	
-'m7' : 'Change From 50 Day Moving Average'
-'h'  : 'Day’s High'	
-'m8' : 'Percent Change From 50 Day Moving Average'
-'k1' : 'Last Trade-Realtime-With Time'
-'m3' : '50 Day Moving Average'
-'l'  : 'Last Trade-With Time-'
-'m4' : '200 Day Moving Average'
-'l1' : 'Last Trade-Price Only-'
-'t8' : '1 yr Target Price'
+'After Hours Change-Realtime'                   :  'm5',
+'Change From 200 Day Moving Average'        	:  'c3', 
+'Commission'	                                :  'm6',
+'Percent Change From 200 Day Moving Average' 	:  	'g',
+'Day’s Low'	                                	:  'm7',
+'Change From 50 Day Moving Average'          	:  	'h',
+'Day’s High'	                                :  'm8',
+'Percent Change From 50 Day Moving Average'  	:  'k1',
+'Last Trade-Realtime-With Time'              	:  'm3',
+'50 Day Moving Average'                      	:  	'l',
+'Last Trade-With Time-'                      	:  'm4',
+'200 Day Moving Average'                     	:  'l1',
+'Last Trade-Price Only-'                     	:  't8',
+'1 yr Target Price'
 
 # Misc #
 # ----- #
-'w1' : 'Day’s Value Change'
-'g1' : 'Holdings Gain Percent'
-'w4' : 'Day’s Value Change (Realtime)'
-'g3' : 'Annualized Gain'
-'p1' : 'Price Paid'
-'g4' : 'Holdings Gain'
-'m'  : 'Day’s Range'
-'g5' : 'Holdings Gain Percent (Realtime)'
-'m2' : 'Day’s Range (Realtime)'
-'g6' : 'Holdings Gain (Realtime)'
+'Day’s Value Change'                            :  'w1',
+'Holdings Gain Percent'                         :  'g1',
+'Day’s Value Change (Realtime)'                 :  'w4',
+'Annualized Gain'                           	:  'g3',
+'Price Paid'                                	:  'p1',
+'Holdings Gain'                             	:  'g4',
+'Day’s Range'                               	:   'm', 
+'Holdings Gain Percent (Realtime)'          	:  'g5',
+'Day’s Range (Realtime)'                    	:  'm2',
+'Holdings Gain (Realtime)'                  	:  'g6',
 
 # 52 Week Pricing Symbol Info #
 # --------------------------- #
-'k'  : '52 Week High'
-'v'  : 'More Info'
-'j'  : '52 week Low'
-'j1' : 'Market Capitalization'
-'j5' : 'Change From 52 Week Low'
-'j3' : 'Market Cap (Realtime)'
-'k4' : 'Change From 52 week High'
-'f6' : 'Float Shares'
-'j6' : 'Percent Change From 52 week Low'
-'n'  : 'Name'
-'k5' : 'Percent Change From 52 week High'
-'n4' : 'Notes'
-'w'  : '52 week Range'
-'s'  : 'Symbol'
-'s1' : 'Shares Owned'
-'x'  : 'Stock Exchange'
-'j2' : 'Shares Outstanding'
+'52 Week High'                                  :   'k',  
+'More Info'                                     :   'v',  
+'52 week Low'                                  	:   'j',  
+'Market Capitalization'                        	:  'j1',
+'Change From 52 Week Low'                      	:  'j5', 
+'Market Cap (Realtime)'                        	:  'j3', 
+'Change From 52 week High'                      :  'k4', 
+'Float Shares'                                  :  'f6', 
+'Percent Change From 52 week Low'               :  'j6',
+'Name'                                          :   'n',  
+'Percent Change From 52 week High'              :  'k5',
+'Notes'                                         :  'n4', 
+'52 week Range'                                 :   'w',  
+'Symbol'                                        :   's',  
+'Shares Owned'                                  :  's1', 
+'Stock Exchange'                                :   'x',  
+'Shares Outstanding'                            :  'j2', 
 
 # Volume #
 # ------ #
-'v'  : 'Volume'
-'a5' : 'Ask Size'	
-'b6' : 'Bid Size'
-'k3' : 'Last Trade Size'	
-'t7' : 'Ticker Trend'
-'a2' : 'Average Daily Volume'	
-'t6' : 'Trade Links'
-'i5' : 'Order Book (Realtime)'
+'Volume'                                        :   'v', 
+'Ask Size'	                                    :  'a5',
+'Bid Size'                                      : 'bi6',
+'Last Trade Size'	                            :  'k3',
+'Ticker Trend'                                  :  't7',
+'Average Daily Volume'	                        :  'a2',
+'Trade Links'                                   :  't6',
+'Order Book (Realtime)'                         :  'i5',
 
 # Ratios #
 # ------ #
-'l2' : 'High Limit'
-'e'  : 'Earnings per Share'
-'l3' : 'Low Limit'
-'e7' : 'EPS Estimate Current Year'
-'v1' : 'Holdings Value'
-'e8' : 'EPS Estimate Next Year'	
-'v7' : 'Holdings Value (Realtime)'
-'e9' : 'EPS Estimate Next Quarter'	
-'s6' : 'Revenue'
-'b4' : 'Book Value'
-'j4' : 'EBITDA'
-'p5' : 'Price / Sales'
-'p6' : 'Price / Book'
-'r'  : 'P/E Ratio'
-'r2' : 'P/E Ratio (Realtime)'
-'r5' : 'PEG Ratio'
-'r6' : 'Price / EPS Estimate Current Year'
-'r7' : 'Price / EPS Estimate Next Year'
-'s7' : 'Short Ratio'
-
+ 'High Limit'                                   :  'l2',
+ 'Earnings per Share'                           :   'e',  
+ 'Low Limit'                                    :  'l3', 
+ 'EPS Estimate Current Year'                    :  'e7', 
+ 'Holdings Value'                               :  'v1', 
+ 'EPS Estimate Next Year'	                    :  'e8', 
+ 'Holdings Value (Realtime)'                    :  'v7', 
+ 'EPS Estimate Next Quarter'	                :  'e9', 
+ 'Revenue'                                      :  's6', 
+ 'Book Value'                                   :  'b4', 
+ 'EBITDA'                                       :  'j4', 
+ 'Price / Sales'                                :  'p5', 
+ 'Price / Book'                                 :  'p6', 
+ 'P/E Ratio'                                    :  'r' ,
+ 'P/E Ratio (Realtime)'                         :  'r2', 
+ 'PEG Ratio'                                    :  'r5', 
+ 'Price / EPS Estimate Current Year'            :  'r6', 
+ 'Price / EPS Estimate Next Year'               :  'r7', 
+ 'Short Ratio'                                  : 's7' 
 }
